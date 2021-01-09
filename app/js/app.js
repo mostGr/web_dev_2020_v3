e2ffab3fc02ddd5cbe8ad5c0e3effcc27d4458b6
Ilshat <ilshatanet@gmail.com>
2021-01-10 01:08:01 +0800

    ended carousel section

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.carousel-inner').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icon/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icon/right.svg"></button>',
+        autoplay: true
+    });
+
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ function browsersync() {
 
 function scripts() {
     return src([
-            'node_modules/jquery/dist/jquery.min.js',
+            // 'node_modules/jquery/dist/jquery.min.js',
             'app/js/app.js' // Пользовательские скрипты, использующие библиотеку
         ])
         .pipe(concat('app.min.js'))
